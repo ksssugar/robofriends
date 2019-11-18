e3c7b17e992b2343569608de7d9c3909fe71fbe5
Neo <ksssugar@gmail.com>
2019-11-19 01:06:42 +0800

    Updates

---
+++ precache-manifest.eb369ec02c26f0558364b32ca14a57c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc77acb3327fdc4fd92276d933abd76",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "31f12ee6abfafa8893a3",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9844fcf2bc50c7bf8576",
+    "url": "/robofriends/static/css/main.f13d8851.chunk.css"
+  },
+  {
+    "revision": "31f12ee6abfafa8893a3",
+    "url": "/robofriends/static/js/2.52f690f9.chunk.js"
+  },
+  {
+    "revision": "9844fcf2bc50c7bf8576",
+    "url": "/robofriends/static/js/main.9822d755.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
